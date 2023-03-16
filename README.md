195e9b47d8e750596c79227ff7bb5d56678ed951
Pierce <pierceevans@me.com>
2023-03-16 13:39:44 -0400

    firstCommit

---
+++ README.md
@@ -0,0 +1,10 @@
+**Simple Weather Forecatst SPA**
+
+This application was designed to display the weather forecast over the next 12 hours. Once the app has opened, it will fade in over 6 seconds.
+
+After the viewer has been greeeted, they have the option to get the local forecast with a mouseover event, giving them a disclaimer of what will happen once the button has been pressed.
+
+Finally, After the viewer has chosen to get the forecast, the application fetches the weather data from https://open-meteo.com/ given the longitude and latitude at the top of the getForecast function, and displays the hourly forecast, windspeed and humidity at 2 meters above ground level in 12 sections for the next 12 hours.
+
+For more information on how how the code functions visit: https://medium.com/@pierceevans_30049/phase-1-completion-thoughts-2e28c32f731b
+Hello world
---
+++ index.js
@@ -0,0 +1,57 @@
+
+function mouseOver() {
+    const infoLabelElement = document.getElementById("infoLabel")
+    infoLabelElement.innerHTML = "Get hourly forecast, humidity and wind speed"
+}
+function mouseOut() {
+    const infoLabelElement = document.getElementById("infoLabel")
+    infoLabelElement.innerHTML = ""
+}
+
+function getForecast() {
+    var latitude = (39.77)
+    var longitude = (-86.16)
+    var startDate = new Date().toISOString().split('T')[0]
+    var endDate = new Date(new Date().setDate(new Date().getDate() + 1)).toISOString().split('T')[0]
+
+    var url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true&hourly=temperature_2m,relativehumidity_2m,windspeed_10m&start_date=${startDate}&end_date=${endDate}`
+    var forecastDiv = document.getElementById("forecast")
+
+
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            /*console.log(data)
+
+            //console.log(data.current_weather.temperature)
+            console.log(data.hourly.time)*/
+            forecastDiv.innerHTML = `<div class = "currentWeatherTemp"> Current Temp: ${data.current_weather.temperature * 1.8 + 32}<sup>o</sup> F </div>`
+            data.hourly.time.forEach((element,i) => {
+                if (
+                    new Date() < new Date(data.hourly.time[i]) &&
+                    new Date(data.hourly.time[i]) < new Date(new Date().setHours(new Date().getHours() + 12))
+                ) {
+                    const mainDiv = document.createElement("div")
+                    var celciusTemp = Math.floor(data.hourly.temperature_2m[i] * 1.8 + 32)
+
+                    var msgString = `<div class="temps">Hour ${i}:<br>
+                              Temperature: ${celciusTemp}<sup>o</sup> F <br>
+
+                              Relative Humidity: ${data.hourly.relativehumidity_2m[i]}%<br>
+                              Wind Speed: ${data.hourly.windspeed_10m[i]}mph</div>
+                            `
+
+                    mainDiv.innerHTML = msgString
+                    forecastDiv.appendChild(mainDiv);
+                }
+            });
+        }).catch(e => alert(e));
+}
+
+const buttonElement = document.getElementById("buttonId")
+buttonElement.addEventListener("click", getForecast)
+buttonElement.addEventListener("mouseover", mouseOver)
+buttonElement.addEventListener("mouseout", mouseOut)
+
+
+//mouseOver()
