aa07d2d2e48bfcd5c8ccda4bee61e532a6b122d9
Pierce <pierceevans@me.com>
2023-03-18 11:08:56 -0400

    remove single line comment

--- index.js
+++ index.js
@@ -20,9 +20,9 @@ function getForecast() {
     fetch(url)
         .then((response) => response.json())
         .then((data) => {
-           
 
-            //console.log(data.current_weather.temperature)
+
+            console.log(data.current_weather.temperature)
             console.log(data.hourly.time)
             forecastDiv.innerHTML = `<div class = "currentWeatherTemp"> Current Temp: ${Math.floor(data.current_weather.temperature) * 1.8 + 32}<sup>o</sup> F </div>`
             data.hourly.time.forEach((element,i) => {
